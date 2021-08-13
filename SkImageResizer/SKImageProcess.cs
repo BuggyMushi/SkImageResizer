0ea40a6597e53b255e51f209cc1b62e163b147da
ladybird@mirle.com.tw <ladybird@mirle.com.tw>
2021-08-13 23:44:48 +0800

    Revert "初版"
    This reverts commit c744751d1ab15a5b4848c8e02f88c08bd2875128.

--- SkImageResizer/SKImageProcess.cs
+++ SkImageResizer/SKImageProcess.cs
@@ -54,67 +54,28 @@ namespace SkImageResizer
             }
 
             await Task.Yield();
-            var LstAsync = new List<Task>();
+
             var allFiles = FindImages(sourcePath);
             foreach (var filePath in allFiles)
             {
-                //var bitmap = SKBitmap.Decode(filePath);
-                //var imgPhoto = SKImage.FromBitmap(bitmap);
-                //var imgName = Path.GetFileNameWithoutExtension(filePath);
+                var bitmap = SKBitmap.Decode(filePath);
+                var imgPhoto = SKImage.FromBitmap(bitmap);
+                var imgName = Path.GetFileNameWithoutExtension(filePath);
 
-                //var sourceWidth = imgPhoto.Width;
-                //var sourceHeight = imgPhoto.Height;
-                //var destinationWidth = (int)(sourceWidth * scale);
-                //var destinationHeight = (int)(sourceHeight * scale);
+                var sourceWidth = imgPhoto.Width;
+                var sourceHeight = imgPhoto.Height;
 
-                //using var scaledBitmap = bitmap.Resize(
-                //    new SKImageInfo(destinationWidth, destinationHeight),
-                //    SKFilterQuality.High);
-                //using var scaledImage = SKImage.FromBitmap(scaledBitmap);
-                //using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-                //using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
-                //data.SaveTo(s);
-                LstAsync.Add(Task.Run(async () =>
-                {
-                    var 圖片名稱 = Path.GetFileNameWithoutExtension(filePath);
-                    Task<SKData> 欲儲存檔案 = Task.Run(() => 轉化過程Async(filePath, scale));
-                    var 儲存位置 = File.OpenWrite(Path.Combine(destPath, 圖片名稱 + ".jpg"));
-                    var 檔案 = await 欲儲存檔案;
-                    檔案.SaveTo(儲存位置);
-                }
-                )
-                );
+                var destinationWidth = (int)(sourceWidth * scale);
+                var destinationHeight = (int)(sourceHeight * scale);
+
+                using var scaledBitmap = bitmap.Resize(
+                    new SKImageInfo(destinationWidth, destinationHeight),
+                    SKFilterQuality.High);
+                using var scaledImage = SKImage.FromBitmap(scaledBitmap);
+                using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
+                using var s = File.OpenWrite(Path.Combine(destPath, imgName + ".jpg"));
+                data.SaveTo(s);
             }
-            await Task.WhenAll(LstAsync);
-        }
-
-        private async Task<SKData> 轉化過程Async(string 來源位置, double 縮放大小)
-        {
-            //var bitmap = SKBitmap.Decode(來源位置);
-            //var imgPhoto = SKImage.FromBitmap(bitmap);
-            //var imgName = Path.GetFileNameWithoutExtension(來源位置);
-
-            //var sourceWidth = imgPhoto.Width;
-            //var sourceHeight = imgPhoto.Height;
-            //var destinationWidth = (int)(sourceWidth * 縮放大小);
-            //var destinationHeight = (int)(sourceHeight * 縮放大小);
-
-            //using var scaledBitmap = bitmap.Resize(
-            //    new SKImageInfo(destinationWidth, destinationHeight),
-            //    SKFilterQuality.High);
-            //using var scaledImage = SKImage.FromBitmap(scaledBitmap);
-            //using var data = scaledImage.Encode(SKEncodedImageFormat.Jpeg, 100);
-            //return data;
-
-            Task<SKBitmap> 原圖解碼 = Task.Run(() => SKBitmap.Decode(來源位置));
-            var 解碼圖 = await 原圖解碼;
-            var 圖片 = SKImage.FromBitmap(解碼圖);
-            Task<SKBitmap> 大小調整圖 = Task.Run(() => 解碼圖.Resize
-            (new SKImageInfo((int)(圖片.Width * 縮放大小), (int)(圖片.Height * 縮放大小)), SKFilterQuality.High));
-
-            Task<SKImage> 已調整大小圖 = Task.Run(async () => SKImage.FromBitmap(await 大小調整圖));
-            Task<SKData> 編碼圖 = Task.Run(async () => (await 已調整大小圖).Encode(SKEncodedImageFormat.Jpeg, 100));
-            return await 編碼圖;
         }
 
         /// <summary>
@@ -145,13 +106,10 @@ namespace SkImageResizer
         /// <returns></returns>
         public List<string> FindImages(string srcPath)
         {
-            //var LstTask = new List<Task>();
-
             List<string> files = new List<string>();
             files.AddRange(Directory.GetFiles(srcPath, "*.png", SearchOption.AllDirectories));
             files.AddRange(Directory.GetFiles(srcPath, "*.jpg", SearchOption.AllDirectories));
             files.AddRange(Directory.GetFiles(srcPath, "*.jpeg", SearchOption.AllDirectories));
-            //await Task.WhenAll(LstTask);
             return files;
         }
     }
